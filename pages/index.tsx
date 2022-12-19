e44c01213c085c9691846efb42fca9c651bcf9da
Tom Sonntag <tom.sonntag@sap.com>
2022-12-19 15:15:06 +0100

    beta new

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,19 @@
 import Head from 'next/head'
+import Navbar from "../components/Navbar";
 //import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
 //const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>sundayish</title>
-        <meta name="description" content="Tom Sonntags personal website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/portrait_tom.png" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <h1>beta</h1>
-        </div>
-
-        <div className={styles.description}>
-          <p>
-            whilst this is building,<br/> get in touch with me:&nbsp;
-            <code className={styles.code}><a href="mailto:tom@sundayish.dev">tom@sundayish.dev</a></code>
-          </p>
-        </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>sundayish</title>
+                <meta name="description" content="Tom Sonntags personal website"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/portrait_tom.png"/>
+            </Head>
+            <Navbar/>
+        </>
+    )
 }
