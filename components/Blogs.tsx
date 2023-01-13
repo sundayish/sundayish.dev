5ffcf477660e72cb1e4725b96301b9ff5e9fa512
Tom Sonntag <tm.snntag@icloud.com>
2023-01-13 12:18:59 +0100

    Add: Blog prototype

---
+++ components/Blogs.tsx
@@ -0,0 +1,110 @@
+export default function Blogs() {
+    return (
+        <>
+            <div className="">
+                <h2 className="text-4xl leading-10 tracking-tight font-bold text-sky-50 text-center">Blogs</h2>
+                <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
+                    <div className="bg-gray-500 rounded-lg shadow-lg overflow-hidden">
+                        <div>
+                            <img className="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1461773518188-b3e86f98242f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt=""/>
+                        </div>
+                        <div className="p-6">
+                            <p className="text-sm leading-5 font-semibold text-indigo-600">Blog</p>
+                            <h3 className="text-xl leading-7 font-bold text-sky-100">
+                                Blog title
+                            </h3>
+                            <p className="mt-3 text-base leading-6 text-sky-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed.</p>
+                            <div className="mt-6 flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img className="h-10 w-10 rounded-full bg-gray-800" src="/portrait_tom_big.png" alt=""/>
+                                </div>
+                                <div className="ml-3">
+                                    <p className="text-sm leading-5 font-semibold text-sky-50">
+                                        Tom Sonntag
+                                    </p>
+                                    <div className="flex text-sm leading-5 text-sky-100">
+                                        <time dateTime="2023-01-13">
+                                            Jan 01, 2023
+                                        </time>
+                                        <span className="mx-1">
+                                            &middot;
+                                        </span>
+                                        <span>
+                                            3 min read
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-500 rounded-lg shadow-lg overflow-hidden">
+                        <div>
+                            <img className="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1461773518188-b3e86f98242f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt=""/>
+                        </div>
+                        <div className="p-6">
+                            <p className="text-sm leading-5 font-semibold text-indigo-600">Blog</p>
+                            <h3 className="text-xl leading-7 font-bold text-sky-100">
+                                Blog title
+                            </h3>
+                            <p className="mt-3 text-base leading-6 text-sky-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed.</p>
+                            <div className="mt-6 flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img className="h-10 w-10 rounded-full bg-gray-800" src="/portrait_tom_big.png" alt=""/>
+                                </div>
+                                <div className="ml-3">
+                                    <p className="text-sm leading-5 font-semibold text-sky-50">
+                                        Tom Sonntag
+                                    </p>
+                                    <div className="flex text-sm leading-5 text-sky-100">
+                                        <time dateTime="2023-01-13">
+                                            Jan 01, 2023
+                                        </time>
+                                        <span className="mx-1">
+                                            &middot;
+                                        </span>
+                                        <span>
+                                            3 min read
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-500 rounded-lg shadow-lg overflow-hidden">
+                        <div>
+                            <img className="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1461773518188-b3e86f98242f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt=""/>
+                        </div>
+                        <div className="p-6">
+                            <p className="text-sm leading-5 font-semibold text-indigo-600">Blog</p>
+                            <h3 className="text-xl leading-7 font-bold text-sky-100">
+                                Blog title
+                            </h3>
+                            <p className="mt-3 text-base leading-6 text-sky-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed.</p>
+                            <div className="mt-6 flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img className="h-10 w-10 rounded-full bg-gray-800" src="/portrait_tom_big.png" alt=""/>
+                                </div>
+                                <div className="ml-3">
+                                    <p className="text-sm leading-5 font-semibold text-sky-50">
+                                        Tom Sonntag
+                                    </p>
+                                    <div className="flex text-sm leading-5 text-sky-100">
+                                        <time dateTime="2023-01-13">
+                                            Jan 01, 2023
+                                        </time>
+                                        <span className="mx-1">
+                                            &middot;
+                                        </span>
+                                        <span>
+                                            3 min read
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- components/Epic.tsx
+++ components/Epic.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Profile from "./Profile";
+import Blogs from "./Blogs";
 
 export default function Epic(){
     return(
@@ -7,6 +8,7 @@ export default function Epic(){
             <div className="h-screen w-5/6 mx-auto">
                 <Navbar />
                 <Profile />
+                <Blogs />
             </div>
 
         </>
